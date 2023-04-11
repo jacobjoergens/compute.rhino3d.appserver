be87f492439ef3bbdaeab62cda62f6f63887fbaf
jacobjoergens <jacob.joergens@gmail.com>
2023-04-11 10:34:33 -0400

    generate buffered SVG graphs
    handles all possible maximum independent sets instead of just one by outputting figures for each bipartite graph

--- src/examples/clt-project/degenerateDecomposition.py
+++ src/examples/clt-project/degenerateDecomposition.py
@@ -2,13 +2,18 @@ import networkx as nx
 import numpy as np
 from itertools import combinations
 from ingest import cornerList
+import matplotlib.pyplot as plt
+import os
+import json
+import base64
+import io
 
 def getDegCorners(corner_lists,deg_chord,dir):
     opdir = (dir+1)%2
     a_corner = deg_chord[0]
     b_corner = deg_chord[1]
-    a_index = a_corner.index
-    b_index = b_corner.index
+    a_index = a_corner.list_index
+    b_index = b_corner.list_index
     if(b_index==a_index):
         if(a_corner.vertex[opdir]!=a_corner.prev.vertex[opdir]):
             return b_corner, a_corner, b_index, a_index
@@ -27,8 +32,14 @@ def stageDegDecompGeometry(corner, horver, dir):
         backward = "next"
     return forward, backward
 
-def decompose(horizontal, vertical, intersections, corner_lists):
+def createBranches(independent_set):
+    return None 
+
+def getMaxIndependentSet(horizontal, vertical, intersections, corner_lists):
+    k = 4
+    max_sets = []
     independent_sets = set()
+    G = None
     if(len(intersections)>0):
         G = nx.Graph()
         B = nx.Graph()
@@ -37,10 +48,16 @@ def decompose(horizontal, vertical, intersections, corner_lists):
         i = 0
         hz_intersects = set()
         b_top = []
+        h_counter = 1 
+        v_counter = 1 
         for ii in intersections:
             hz_intersects.add(ii[0])
-            G.add_node(ii[0],bipartite=0)
-            G.add_node(ii[1],bipartite=1)
+            if(not G.has_node(ii[0])):
+                G.add_node(ii[0],bipartite=0,label=f"H{h_counter}")
+                h_counter+=1
+            if(not G.has_node(ii[1])):
+                G.add_node(ii[1],bipartite=1,label=f"V{v_counter}")
+                v_counter+=1
             if(ii[0] not in b_dict):
                 b_dict[ii[0]]=chr(char)
                 b_top.append(chr(char))
@@ -58,10 +75,71 @@ def decompose(horizontal, vertical, intersections, corner_lists):
         for combo in combos: 
             if not any(G.has_edge(u,v) for u,v in combinations(combo, 2)):
                 independent_sets.add(combo)
-        max_set = horizontal+vertical+list(list(independent_sets)[-1]) #taking last for now, change later when allowing different max covers
+
+        for independent_set in list(independent_sets):
+            max_sets.append(horizontal+vertical+list(independent_set))
     else: 
-        max_set = horizontal+vertical   
-    
+        max_sets = [horizontal+vertical] 
+
+    if k>4: 
+        for independent_set in max_sets:
+            createBranches(independent_set)
+
+    return max_sets, G, top, bottom, h_counter, v_counter
+
+def generateGraphs(max_sets, G, top, bottom, horizontal, vertical, h_counter, v_counter):
+    top_len = len(list(top))
+    bottom_len = len(list(bottom))
+    for j in range(len(horizontal)):
+        G.add_node(horizontal[j],biparite=0,label=f"H{h_counter+j}")
+
+    for k in range(len(vertical)):
+        G.add_node(vertical[k],biparite=1,label=f"V{v_counter+k}")
+
+    labels = nx.get_node_attributes(G, "label")
+    node_list = list(G.nodes)
+    pos = {}
+    label_pos = {}
+
+    max_part_len = max(top_len+len(horizontal),bottom_len+len(vertical))-1
+    step = 9/max_part_len
+    if(bottom_len>top_len):
+        h,v = (bottom_len-top_len)/2*step,0
+    elif(top_len>bottom_len):
+        h,v = 0,(top_len-bottom_len)/2*step
+    else: 
+        h,v = 0,0
+    for node, label in sorted(labels.items(),key=lambda i: int(i[1][1:])):
+        if(label[0]=='H'):
+            pos[node] = (0.1,h)
+            label_pos[node] = (pos[node][0] - 0.2, pos[node][1])
+            h+=step
+        elif(label[0]=='V'):
+            pos[node] = (0.9,v)
+            label_pos[node] = (pos[node][0] + 0.2, pos[node][1])
+            v+=step
+
+    bipartite_figures = []
+    for i in range(len(max_sets)):
+        plt.figure(figsize=(2,9))
+        node_color = {node: 'w' if node in max_sets[i] else 'none' for node in node_list}
+        nx.draw_networkx_nodes(G, pos, node_size= min(200, 290-max_part_len*15),nodelist=node_list, node_shape='o', edgecolors='w', node_color=[node_color[node] for node in node_list])
+        nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5, edge_color='gray')
+        nx.draw_networkx_labels(G, label_pos, labels, font_size=min(9, 9.9-max_part_len*0.15), font_family="monospace", font_color='w')
+        plt.xlim(-0.25, 1.25)
+        plt.axis('off')
+        plt.gca().set_aspect(2/9)
+        # Convert the plot to a SVG image in memory
+        buf = io.BytesIO()
+        plt.savefig(buf, bbox_inches='tight', format='svg', transparent=True)
+        buf.seek(0)
+
+        # Encode the SVG image as a base64 string
+        b64_bytes = base64.b64encode(buf.read())
+        bipartite_figures.append(b64_bytes.decode('utf-8'))
+    return bipartite_figures
+
+def decompose(max_set, corner_lists):
     horver = ["horizontal","vertical"]
     for deg_chord in max_set:
         chord_vector = np.array(deg_chord[1].vertex)-np.array(deg_chord[0].vertex)
@@ -123,7 +201,5 @@ def decompose(horizontal, vertical, intersections, corner_lists):
             corner_lists.append(b_list)
         else: 
             corner_lists.pop(b_index)
-        # a_list.updateState()
-        # for i in range(len(corner_lists)):
-        #     print(i, dir, corner_lists[i].length, corner_lists[i].concave_count)
-    return corner_lists, G
+
+    return corner_lists
