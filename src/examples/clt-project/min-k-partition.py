7bd43459905f0158a9f6dc0e476154f7e3582f56
jacobjoergens <jacob.joergens@gmail.com>
2023-04-19 15:45:08 -0400

    add getPartition()
    allow retrieval of individual non-degenerate partitions

--- src/examples/clt-project/min-k-partition.py
+++ src/examples/clt-project/min-k-partition.py
@@ -5,21 +5,23 @@ import sys
 import json
 import asyncio
 import websockets
-import signal 
-import socket
-import queue 
-import os
+import itertools
+import copy
 
 sys.stdout.flush()
 
 corner_lists = None
 k = 0
+deg_partitions = {}
+dir_patterns = None 
+concave_corners = None
+max_sets = []
 
 def stagePartitioning(input):
-    global corner_lists, k
+    global corner_lists, k, max_sets
     curve = input['crvPoints']
     k = input['k']
-    # deg_set_num = input['deg_set_num']
+    
     corner_lists, max_extent = ingest.digestCurves(curve)
     concave_corners = ingest.findConcaveVertices(corner_lists)
     
@@ -28,17 +30,64 @@ def stagePartitioning(input):
     intersections = ingest.findIntersections(horizontal,vertical)
     max_sets, G, top, bottom, h_counter, v_counter = degenerateDecomposition.getMaxIndependentSet(horizontal,vertical,intersections,corner_lists)
     bipartite_figures = degenerateDecomposition.generateGraphs(max_sets, G, top, bottom, horizontal, vertical, h_counter, v_counter)
-    return max_sets, bipartite_figures
 
-def getPartition(max_set): 
-    global corner_lists, k
-    corner_lists = degenerateDecomposition.decompose(max_set,corner_lists)
-    
+    return bipartite_figures
+
+def getConcaveCorners(cornerLists):
+    concave_corners = []
+    for corner_list in cornerLists:
+        current_corner = corner_list.head
+        if(current_corner.concave):
+            concave_corners.append(current_corner)
+        current_corner = current_corner.next
+        while(current_corner!=corner_list.head):
+            if(current_corner.concave):
+                concave_corners.append(current_corner)
+            current_corner = current_corner.next
+    return concave_corners
+
+def getPartition(input): 
+    global corner_lists, k, deg_partitions
+    degSetIndex = input['degSetIndex']
+    index = input['index']
+
+    if(degSetIndex not in deg_partitions): 
+        deg_corner_lists = []
+        conversionTable = {}
+        for corner_list in corner_lists:
+            deg_corner_lists.append(corner_list.copyList(conversionTable))
+
+        max_set = []
+        for deg_chord in max_sets[degSetIndex]:
+            max_set.append((conversionTable[deg_chord[0]],conversionTable[deg_chord[1]]))
+
+        nonDegBasis = degenerateDecomposition.decompose(max_set,deg_corner_lists)
+
+        concave_corners = getConcaveCorners(nonDegBasis)
+        deg_partitions[degSetIndex] = { 
+            'set': nonDegBasis,
+            'concave_corners': concave_corners,
+            'dir_patterns': list(itertools.product([0, 1], repeat=len(concave_corners)))
+        }
+        
+
     regions = []
-    nondegenerateDecomposition.decompose(0, corner_lists, regions, k)
+    degSet = deg_partitions[degSetIndex]
+    conversionTable = {}
+    non_deg_corner_lists = []
+    for corner_list in degSet['set']: 
+        non_deg_corner_lists.append(corner_list.copyList(conversionTable))
+    
+
+
+    concave_corners = []
+    for corner in degSet['concave_corners']:
+        concave_corners.append(conversionTable[corner])
+    nondegenerateDecomposition.decompose(degSet['dir_patterns'][index], concave_corners, non_deg_corner_lists, regions, k)
     data = {
         'regions': regions,
-        'k': k
+        'k': k,
+        'numNonDegParts': degSet['dir_patterns']
     }
     return data
     
@@ -52,17 +101,23 @@ async def handle_client(websocket, path):
         params = data.get('params', [])
 
         if action == 'stage':
-            max_sets, bipartite_figures = stagePartitioning(params[0])
+            bipartite_figures = stagePartitioning(params[0])
             message = {'type': 'stage', 
                        'message': {
-                            'max_sets': len(max_sets), 
-                            'bipartite_figures':bipartite_figures}}
+                            # 'max_sets': len(max_sets), 
+                            'bipartite_figures':bipartite_figures
+                            }
+                        }
             await websocket.send(json.dumps(message))
         elif action == 'get':
-            data = getPartition(max_sets[params[0]])
+            data = getPartition(params[0])
             message = {'type': 'get', 
-                       'message': f"length of corner_list at index {params[0]}: {len(data['regions'])}",
-                       'body': data}
+                       'message': {
+                            'regions': data['regions'],
+                            'k': data['k'],
+                            'numNonDegParts':data['numNonDegParts']
+                        }
+                       }
             await websocket.send(json.dumps(message))
 
 async def start_server():
